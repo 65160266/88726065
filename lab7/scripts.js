3c05d0c86ed1c17c0991bb75ff11723666db3765
65160266 <65160266@go.buu.ac.th>
2024-01-17 19:05:24 +0700

    comment#5
    ‘toggle รายการ Todo’

--- lab7/scripts.js
+++ lab7/scripts.js
@@ -25,7 +25,11 @@ document.addEventListener("DOMContentLoaded", function () {
     renderTodoList();
     }
 
-   
+    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodoList();
+    }
 
 // แสดงรายการ Todo บนหน้าเว็บ
 function renderTodoList() {
